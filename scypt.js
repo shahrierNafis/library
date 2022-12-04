78e8fc54945a8df5d3e7b0d2e2410cb2bd92f16e
shahrierNafis <shahriernafisog@gmail.com>
2022-12-04 09:49:38 +0600

    Use class

--- scypt.js
+++ scypt.js
@@ -1,10 +1,12 @@
 let myLibrary = [];
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    Book.prototype.info = function () {
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+    }
+    info() {
         return `${title}+ by ${author}, ${pages} pages, ${this.read}`;
     }
 }
